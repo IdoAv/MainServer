d25b1779ad7dfdb0a74a773a0507543d0e13b526
User <User@LAB3-17>
2021-09-13 14:27:12 +0300

    Add project files.

---
+++ MainServer/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MainServerBL.Models;
+namespace MainServer.Controllers
+{
+    [Route("MainAPI")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        MainDBContext context;
+        public MainController(MainDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
